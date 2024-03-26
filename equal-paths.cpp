ab1bd3378868e5f0027f9cc36f9b8631ae0f1828
Alex Chen <achen715@usc.edu>
2024-03-26 03:50:18 +0000

    commit message

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,7 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
+#include<algorithm>
 #endif
 
 #include "equal-paths.h"
@@ -13,6 +14,49 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+  if(root == nullptr)
+  {
+    return true;
+  }
 
+  // both children null, return true
+  if (root->left == nullptr && root->right == nullptr)
+  {
+    return true;
+  }
+  
+  // one of the children null, the other not, reuturn false
+  if (root->left == nullptr && root->right != nullptr)
+  {
+    return equalPaths(root->right);
+  }
+
+  if(root->right == nullptr && root->left != nullptr)
+  {
+    return equalPaths(root->left);
+  }\
+
+  // first check the whole tree, if the children of the 
+  // root node does not have same length, return false
+  if(rootDepth(root->left) != rootDepth(root->right))
+  {
+    return false;
+  }
+  // recursively check if the left and rightchildren of 
+  // the current node has same length 
+  return equalPaths(root->left) && equalPaths(root->right);
+
+}
+int rootDepth(Node* root)
+{
+  if(root == nullptr)
+  {
+    return 0;
+  }
+
+  int leftDepth = rootDepth(root->left);
+  int rightDepth = rootDepth(root->right);
+
+  return max(leftDepth, rightDepth) + 1;
 }
 
